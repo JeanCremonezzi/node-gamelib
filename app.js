263098048310f325580d5c22e9e57b05d55a8eee
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-03-30 22:54:05 -0300

    feat: Created routes file

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
 const express = require("express");
+const router = require('./src/routes');
 const app = express();
 const port = process.env.SERVER_PORT;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+app.use(router);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
