ecb1b9c156c09b2d96b8539ee424ca609954110c
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-04-01 19:46:09 -0300

    feat: Added games by ID route

--- src/routes/games.js
+++ src/routes/games.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import axios from "axios";
 
-// Get games from API by name
+/** Get API games by name */
 router.get("/games", (req, res) => {
     axios({
         method: "POST",
@@ -12,6 +12,22 @@ router.get("/games", (req, res) => {
             'Client-ID': process.env.CLIENT_ID
         },
         data: `search "${req.query.name}"; fields name; limit 30;`
+    })
+    .then((apiRes) => {
+        res.send(apiRes.data);
+    })
+})
+
+/** Get API games by id */
+router.get("/games/:id", (req, res) => {
+    axios({
+        method: "POST",
+        url: `${process.env.API_URL}/games`,
+        headers: {
+            'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+            'Client-ID': process.env.CLIENT_ID
+        },
+        data: `fields name,platforms.name,release_dates.human; where id = ${req.params.id};`
     })
     .then((apiRes) => {
         res.send(apiRes.data);
