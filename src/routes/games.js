f8de38c6a211224f6dbf9369dda15b0ada04e6d1
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-04-02 00:00:29 -0300

    feat: Added routes to consume the external API

--- src/routes/games.js
+++ src/routes/games.js
@@ -2,6 +2,8 @@ import express from 'express';
 const router = express.Router();
 import axios from "axios";
 
+const FIELDS = "fields name, platforms.name, cover.image_id, release_dates.human, platforms.name, platforms.platform_logo.image_id; limit 25;";
+
 /** Get API games by name */
 router.get("/games", (req, res) => {
     axios({
@@ -11,10 +13,12 @@ router.get("/games", (req, res) => {
             'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
             'Client-ID': process.env.CLIENT_ID
         },
-        data: `search "${req.query.name}"; fields name; limit 30;`
+        data:  `search "${req.query.name}"; 
+                ${FIELDS}
+                where category = (0, 3, 4, 6, 8, 9, 10, 11);`
     })
     .then((apiRes) => {
-        res.send(apiRes.data);
+        res.send(transformData(apiRes.data));
     })
 })
 
@@ -27,11 +31,38 @@ router.get("/games/:id", (req, res) => {
             'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
             'Client-ID': process.env.CLIENT_ID
         },
-        data: `fields name,platforms.name,release_dates.human; where id = ${req.params.id};`
+        data:  `${FIELDS} 
+                where id = ${req.params.id};`
     })
     .then((apiRes) => {
-        res.send(apiRes.data);
+        res.send(transformData(apiRes.data));
     })
 })
+
+const transformData = (data) => {
+    let formatted = [];
+
+    if (data.length > 0) {
+        formatted = data.map((item) => {
+            return {
+                "id": item.id,
+                "name": item.name,
+                "initial_release": item.release_dates == null ? "N/A" : item.release_dates[0].human.slice(-4),
+                "cover": item.cover != null ? `https://images.igdb.com/igdb/image/upload/t_cover_big/${item.cover.image_id}.jpg` : null,
+                "platforms": item.platforms == null ? [] : item.platforms.map((platform) => {
+                    return {
+                        "id": platform.id,
+                        "name": platform.name,
+                        "logo_id": platform.platform_logo.id,
+                        "logo": platform.platform_logo.image_id != null ? `https://images.igdb.com/igdb/image/upload/t_logo_med/${platform.platform_logo.image_id}.jpg` : null,
+                    }
+                })
+
+            }
+        })
+    }
+
+    return formatted;
+}
 
 export { router };
