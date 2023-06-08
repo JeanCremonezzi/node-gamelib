0e574a0d1a39987d121ee5c3497bc6d3044fdf13
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-06-08 18:17:08 -0300

    feat: Add cors

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 require('dotenv').config();
 const router = require('./src/routes/router.js');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials: true,
+}))
+
 app.use(router);
 
 app.listen(port, () => {
